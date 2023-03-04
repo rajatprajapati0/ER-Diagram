22e9e3edc7536395157ec4265a561c36bdfed72d
Rajat <rajatprajapati09@gmail.com>
2023-03-05 05:23:32 +0530

    CURD OPERATION -insert data into  TABLE Employee_Payroll

--- Payroll_SQL_Query.sql
+++ Payroll_SQL_Query.sql
@@ -16,3 +16,11 @@ START        DATE NOT NULL,                        --EMPLOYEE JOINING DATE
 PRIMARY KEY  (ID)                                  --TO MAKE ID AS PRIMARY KEY
 
 );
+
+--UC3***
+--            ***CURD OPERATION***
+
+INSERT INTO  Employee_Payroll (ID,NAME,SALARY,START)VALUES
+(1,'RAJAT',60000,'2023-01-01'),
+(2,'DEEPAK',65000,'2023-02-02'),
+(3,'NARESH',68000,'2023-03-03');
