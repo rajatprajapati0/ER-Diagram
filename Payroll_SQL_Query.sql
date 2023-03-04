aff2ee6cf12fabe0e8fa2834f23f857ed2fd8102
Rajat <rajatprajapati09@gmail.com>
2023-03-05 05:00:03 +0530

    CREATED TABLE Employee_Payroll

--- Payroll_SQL_Query.sql
+++ Payroll_SQL_Query.sql
@@ -5,3 +5,14 @@
 --UC1*
 CREATE DATABASE Payroll_Service --TO CREAT DATA BASE
 USE Payroll_Service             --TO USE Payroll_Service DATA BASE
+
+--UC2**
+CREATE TABLE Employee_Payroll   --TO CREAT A TABEL 
+(
+ID           INT  NOT NULL ,     --UNIQUE ID FOR THE RECORD 
+NAME         VARCHAR(150) NOT NULL ,               --NAME OF THE EMPLOYEE
+SALARY       INT NOT NULL,                         --EMPLOYEE SALARY
+START        DATE NOT NULL,                        --EMPLOYEE JOINING DATE
+PRIMARY KEY  (ID)                                  --TO MAKE ID AS PRIMARY KEY
+
+);
